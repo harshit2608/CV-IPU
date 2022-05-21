1cea78443066ea87a074764b4992533a5fb6b5f1
harshit2608 <harshitbargujar.dev@gmail.com>
2022-05-21 21:46:59 +0530

    Updated Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,48 @@
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import Register from "./pages/Register";
-import AddProduct from "./pages/AddProduct";
+import React, { useContext } from 'react';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Profile from "./pages/Profile";
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from 'react-router-dom';
 
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Product from './pages/Product';
+import Register from './pages/Register';
+import AddProduct from './pages/AddProduct';
+import Messenger from './pages/Messenger';
+
+import Profile from './pages/Profile';
+import { AuthContext } from './context/AuthContext';
 
 const App = () => {
-  // const user = useSelector((state) => state.user.currentUser);
-  const user = false;
-  return (
-  <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element = {user ? <Navigate to="/" /> : <Login />}/>
-        <Route path="/register" element = {user ? <Navigate to="/" /> : <Register />}/>
-        <Route path="product" element={<Product/>}/>
-        <Route path="profile" element={<Profile/>}/>
-        <Route path="addProduct" element={<AddProduct/>}/>
-      </Routes>
-  </Router>
-  );
+    // const user = useSelector((state) => state.user.currentUser);
+    const { user } = useContext(AuthContext);
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route
+                    path="/login"
+                    element={user ? <Navigate to="/" /> : <Login />}
+                />
+                <Route
+                    path="/register"
+                    element={user ? <Navigate to="/" /> : <Register />}
+                />
+                <Route path="product" element={<Product />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="addProduct" element={<AddProduct />} />
+                <Route
+                    path="/messenger"
+                    element={
+                        !user ? <Navigate replace to="/" /> : <Messenger />
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,83 +1,116 @@
-import styled from "styled-components";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+import React, { useContext, useRef } from 'react';
+import { loginCall } from '../apiCalls';
+import { AuthContext } from '../context/AuthContext';
+import { CircularProgress } from '@material-ui/core';
+
+import styled from 'styled-components';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
 
 const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: linear-gradient(
-      rgba(255, 255, 255, 0.1),
-      rgba(255, 255, 255, 0.1)
-    ),
-    url("https://www.dropbox.com/s/0n4yyofg0mrkycs/pexels-henry-%26-co-1939485.jpg?dl=1")
-      center;
-  background-size: cover;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(
+            rgba(255, 255, 255, 0.1),
+            rgba(255, 255, 255, 0.1)
+        ),
+        url('https://www.dropbox.com/s/0n4yyofg0mrkycs/pexels-henry-%26-co-1939485.jpg?dl=1')
+            center;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 const Wrapper = styled.div`
-  width: 25%;
-  padding: 20px;
-  background-color: white;
-  opacity: 0.8;
-  border-radius: 2%;
+    width: 25%;
+    padding: 20px;
+    background-color: white;
+    opacity: 0.8;
+    border-radius: 2%;
 `;
 
 const Title = styled.h1`
-  font-size: 24px;
-  font-weight: 300;
+    font-size: 24px;
+    font-weight: 300;
 `;
 
 const Form = styled.form`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
 
 const Input = styled.input`
-  flex: 1;
-  min-width: 40%;
-  margin: 10px 0;
-  padding: 10px;
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0;
+    padding: 10px;
 `;
 
 const Button = styled.button`
-  width: 40%;
-  border: none;
-  padding: 15px 20px;
-  background-color: teal;
-  color: white;
-  cursor: pointer;
-  margin-bottom: 10px;
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+    margin-bottom: 10px;
 `;
 
 const Link = styled.a`
-  margin: 5px 0px;
-  font-size: 12px;
-  text-decoration: underline;
-  cursor: pointer;
+    margin: 5px 0px;
+    font-size: 12px;
+    text-decoration: underline;
+    cursor: pointer;
 `;
 
-const Login = () => {
-  return (
-    <div>
-    <Navbar/>
-    <Container>
-      <Wrapper>
-        <Title>SIGN IN</Title>
-        <Form>
-          <Input placeholder="username" />
-          <Input placeholder="password" />
-          <Button>LOGIN</Button>
-          <Link>FORGOT PASSWORD?</Link>
-          <Link>CREATE A NEW ACCOUNT</Link>
-        </Form>
-      </Wrapper>
-    </Container>
-      <Footer/>
-    </div>
-  );
-};
+export default function Login() {
+    const email = useRef();
+    const password = useRef();
+    const { isFetching, dispatch } = useContext(AuthContext);
 
-export default Login;
+    const handleClick = (e) => {
+        e.preventDefault();
+        loginCall(
+            { email: email.current.value, password: password.current.value },
+            dispatch
+        );
+    };
+    return (
+        <div>
+            <Navbar />
+            <Container>
+                <Wrapper>
+                    <Title>SIGN IN</Title>
+                    <Form onSubmit={handleClick}>
+                        <Input
+                            placeholder="Email"
+                            type="email"
+                            required
+                            className="loginInput"
+                            ref={email}
+                        />
+                        <Input
+                            placeholder="Password"
+                            type="password"
+                            required
+                            minLength="6"
+                            className="loginInput"
+                            ref={password}
+                        />
+                        <Button type="submit" disabled={isFetching}>
+                            {isFetching ? (
+                                <CircularProgress color="white" size="20px" />
+                            ) : (
+                                'Log In'
+                            )}
+                        </Button>
+                        <Link>FORGOT PASSWORD?</Link>
+                        <Link>CREATE A NEW ACCOUNT</Link>
+                    </Form>
+                </Wrapper>
+            </Container>
+            <Footer />
+        </div>
+    );
+}
